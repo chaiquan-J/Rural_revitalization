63c828ff226fcaed4cc045dd78938ce6db83529c
mmj <1700159418@qq.com>
2023-05-25 23:28:14 +0800

    添加登陆跟注册交互效果

--- src/views/Enroll.vue
+++ src/views/Enroll.vue
@@ -4,23 +4,49 @@
     <div class="conent_Enroll">
       <div class="page_title">注 册</div>
       <div class="input_box">
-        <el-input placeholder="请输入用户名/账号" class="input_user"></el-input>
-        <el-input placeholder="请输入邮箱" class="input_user"></el-input>
+        <el-input
+          @blur="userempty"
+          placeholder="请输入用户名"
+          class="input_user"
+          v-model="username"
+        ></el-input>
+        <p class="prompt" :class="{ opacit: useropa }">{{ usertext }}</p>
+        <el-input
+          placeholder="请输入邮箱"
+          v-model="email"
+          class="input_user"
+          @blur="emailempty"
+        ></el-input>
+        <p class="prompt" :class="{ opacit: emailopa }">{{ emailtext }}</p>
         <el-input
           placeholder="请输入手机号"
           maxlength="11"
           show-word-limit
           class="input_user"
+          @blur="phoneempty"
         ></el-input>
+        <p class="prompt" :class="{ opacit: phonopa }">{{ phonetext }}</p>
         <el-input
           show-password
           placeholder="请输入密码"
           class="input_user"
+          @blur="pwdempty"
         ></el-input>
-        <el-input show-password placeholder="请再次输入密码"></el-input>
+        <p class="prompt" :class="{ opacit: pwdopa }">{{ pwdtext }}</p>
+        <el-input
+          show-password
+          placeholder="请再次输入密码"
+          @blur="pswdempty"
+        ></el-input>
+        <p class="prompt" :class="{ opacit: pswdopa }">{{ pswdtext }}</p>
       </div>
       <div class="button_box">
-        <el-button type="success" plain style="height: 50px; width: 120px">
+        <el-button
+          @click="gologin"
+          type="success"
+          plain
+          style="height: 50px; width: 120px"
+        >
           注册
         </el-button>
       </div>
@@ -31,7 +57,129 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      username: "",
+      usertext: "用户名不能为空！",
+      useropa: false,
+      pwd: "",
+      pswd: "",
+      pwdtext: "密码不能为空！",
+      pswdtext: "请再次输入密码！",
+      pwdopa: false,
+      pswdopa: false,
+      email: "",
+      emailtext: "请输入邮箱！",
+      emailopa: false,
+      phone: "",
+      phonetext: "请输入手机号！",
+      phonopa: false,
+    };
+  },
+  methods: {
+    userempty() {
+      // 正则表达式，验证用户是否输入正确格式
+      var userz = /^[\u4e00-\u9fa5a-zA-Z0-9]{0,12}$/;
+      if (this.$data.username == "") {
+        // 为空则显示提示文字
+        this.$data.useropa = true;
+        this.$data.usertext = "用户名不能为空！";
+        // 返回判断值，在注册按钮调用事件时返回判断条件
+        return false;
+        // 通过空判断进入正则表达式判断
+      } else if (userz.test(this.$data.username)) {
+        // 通过正则表达式验证，隐藏提示文字
+        this.$data.useropa = false;
+        return true;
+      } else {
+        // 未通过正则表达式验证，显示对应的提示文字
+        this.$data.useropa = true;
+        this.$data.usertext =
+          "用户名可包含中文、大小写字母和数字，6-12个字符。";
+        return false;
+      }
+    },
+    emailempty() {
+      var emailz =
+        /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
+      if (this.$data.email == "") {
+        this.$data.emailopa = true;
+        this.$data.emailtext = "请输入邮箱！";
+        return false;
+      } else if (emailz.test(this.$data.email)) {
+        this.$data.emailopa = false;
+        return true;
+      } else {
+        this.$data.emailopa = true;
+        this.$data.emailtext = "请输入有效的邮箱格式！";
+        return false;
+      }
+    },
+    phoneempty() {
+      var phonez =
+        /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+      if (this.$data.phone == "") {
+        this.$data.phonopa = true;
+        this.$data.phonetext = "请输入手机号！";
+        return false;
+      } else if (phonez.test(this.$data.phone)) {
+        this.$data.phonopa = false;
+        return true;
+      } else {
+        this.$data.phonopa = true;
+        this.$data.phonetext = "请输入有效的手机号！";
+        return false;
+      }
+    },
+    pwdempty() {
+      var pwdz = /^[a-zA-Z]\w{5,17}$/;
+      if (this.$data.pwd == "") {
+        this.$data.pwdopa = true;
+        this.$data.pwdtext = "请输入密码！";
+        return false;
+      } else if (pwdz.test(this.$data.pwd)) {
+        this.$data.pwdopa = false;
+        return true;
+      } else {
+        this.$data.pwdopa = true;
+        this.$data.pwdtext =
+          "密码以字母开头，只能包含字母、数字和下划线，6-18字符。";
+        return false;
+      }
+    },
+    pswdempty() {
+      if (this.$data.pswd == "") {
+        this.$data.pswdopa = true;
+        this.$data.pswdtext = "请再次输入密码！";
+        return false;
+      } else if (this.$data.pswd == this.$data.pwd) {
+        this.$data.pswdopa = false;
+        return true;
+      } else {
+        this.$data.pswdopa = true;
+        this.$data.pswdtext = "两次输入密码不相同，请重新输入";
+        return false;
+      }
+    },
+    // 注册按钮点击事件
+    gologin() {
+      // 通过与或非的与判断，全部返回值为true才能跳转到登陆页
+      // 如果有一个为flase就重复调用事件提示
+      if (
+        this.userempty() &&
+        this.emailempty() &&
+        this.phoneempty() &&
+        this.pwdempty() &&
+        this.pswdempty()
+      ) {
+        this.$router.push("/Login");
+      } else {
+        this.userempty();
+        this.emailempty();
+        this.phoneempty();
+        this.pwdempty();
+        this.pswdempty();
+      }
+    },
   },
 };
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,12 +4,30 @@
     <div class="conent">
       <div class="page_title">登 录</div>
       <div class="input_box">
-        <el-input placeholder="请输入用户名/账号" class="input_user"></el-input>
-        <el-input show-password placeholder="请输入密码"></el-input>
-        <p style="margin-top: 20px; color: #00ab25">忘记密码?</p>
+        <el-input
+          placeholder="请输入用户名/账号"
+          v-model="username"
+          class="input_user"
+          @blur="userempty"
+        ></el-input>
+        <p class="prompt" :class="{ opacit: nameopa }">请输入用户名/帐号！</p>
+        <el-input
+          show-password
+          v-model="pwsd"
+          placeholder="请输入密码"
+          @blur="showpwsd"
+        ></el-input>
+        <p class="prompt" :class="{ opacit: pwdoopa }">请输入密码！</p>
+        <p style="margin: 0 auto; margin-top: 10px; color: #00ab25">
+          忘记密码?
+        </p>
       </div>
       <div class="button_box">
-        <el-button type="success" plain style="height: 50px; width: 165px"
+        <el-button
+          type="success"
+          plain
+          style="height: 50px; width: 165px"
+          @click="login"
           >登录</el-button
         >
         <el-button
@@ -28,11 +46,50 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      username: "",
+      pwsd: "",
+      nameopa: false,
+      pwdoopa: false,
+    };
   },
   methods: {
+    // 跳转注册
     goEnrll() {
       this.$router.push("/Enroll");
+    },
+    // 判断用户输入框失去焦点时是否为空
+    userempty() {
+      if (this.$data.username == "") {
+        // 为空则显示提示文字
+        this.$data.nameopa = true;
+        return false;
+      } else {
+        // 输入文字后通过判断，红字消失
+        this.$data.nameopa = false;
+        return true;
+      }
+    },
+    // 密码输入框失去焦点时触发事件
+    showpwsd() {
+      // 判断密码是否为空
+      if (this.$data.pwsd == "") {
+        this.$data.pwdoopa = true;
+        return false;
+      } else {
+        this.$data.pwdoopa = false;
+        return true;
+      }
+    },
+    // 登陆按钮事件
+    login() {
+      // 通过多重if和嵌套if判断，账号和密码输入框是否为空，全通过才可以跳转至首页，否则显示相应的提示文字
+      if (this.userempty() && this.showpwsd()) {
+        this.$router.push("/Home");
+      } else {
+        this.userempty();
+        this.showpwsd();
+      }
     },
   },
 };
